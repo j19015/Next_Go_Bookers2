4f23b30c6ae5adfaf5ca36acb43e4146db5d2fd6
j19015 <j19015@sangi.jp>
2023-07-17 04:35:10 +0900

    Bookスキーマ変更

--- backend/ent/schema/book.go
+++ backend/ent/schema/book.go
@@ -1,6 +1,10 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"time"
+)
 
 // Book holds the schema definition for the Book entity.
 type Book struct {
@@ -9,7 +13,17 @@ type Book struct {
 
 // Fields of the Book.
 func (Book) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("title").
+			NotEmpty(),
+		field.String("body").
+			NotEmpty(),
+		field.Time("created_at").
+			Default(time.Now),
+		field.Time("updated_at").
+			Default(time.Now).
+			UpdateDefault(time.Now),
+	}
 }
 
 // Edges of the Book.
