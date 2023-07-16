a157fc5c08a4fbc8b15410243d4df091466fa2ef
j19015 <j19015@sangi.jp>
2023-07-17 04:33:04 +0900

    Userスキーマ変更

--- backend/ent/schema/user.go
+++ backend/ent/schema/user.go
@@ -1,6 +1,10 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"time"
+)
 
 // User holds the schema definition for the User entity.
 type User struct {
@@ -9,7 +13,21 @@ type User struct {
 
 // Fields of the User.
 func (User) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("email").
+			Unique().
+			NotEmpty(),
+		field.String("password").
+			Sensitive().
+			NotEmpty(),
+		field.String("name").
+			NotEmpty(),
+		field.Time("created_at").
+			Default(time.Now),
+		field.Time("updated_at").
+			Default(time.Now).
+			UpdateDefault(time.Now),
+	}
 }
 
 // Edges of the User.
