8b0990a8190c0fda206d78ba44b64f9285d8e528
j19015 <j19015@sangi.jp>
2023-07-18 00:34:48 +0900

    DB(PostgreSQL)接続し新しいスキーマを作成する

--- backend/main.go
+++ backend/main.go
@@ -9,6 +9,18 @@ import (
 )
 
 func main() {
+
+	//PostgreSQLに接続
+	client, err := ent.Open("postgres", "host=db port=5432 user=postgres dbname=bookers2 password=password sslmode=disable")
+
+	if err != nil {
+			log.Fatalf("failed opening connection to postgres: %v", err)
+	}
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+			log.Fatalf("failed creating schema resources: %v", err)
+	}
+
 	//Ginフレームワークのデフォルトの設定を使用してルータを作成
 	router := gin.Default()
 	
