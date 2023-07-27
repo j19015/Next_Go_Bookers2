38d907c721490b30f1a97cf4efc5f0b91edc5c3e
j19015 <j19015@sangi.jp>
2023-07-28 02:04:37 +0900

    sign_up.tsの処理内容を記述

--- frontend/features/users/api/sign_up.ts
+++ frontend/features/users/api/sign_up.ts
@@ -4,9 +4,32 @@ interface User{
   password: string;
 }
 
-export const sign_up=async(e :User)=>{
+interface Date{
+  error: string;
+  messeage: string;
+}
+
+export const sign_up=async(user :User)=>{
   try{
+    // POSTリクエストを使用してユーザー情報をサーバーに送信
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/sign_up`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    });
+    // サーバーからのレスポンスを取得
+    const data :Date = await response.json();
     
+    // サーバーからのレスポンスに基づいて適切な処理を実行
+    if (response.ok) {
+      // 成功した場合の処理
+      console.log('ユーザーの登録に成功しました！');
+    } else {
+      // エラーが発生した場合の処理
+      console.error('ユーザーの登録に失敗しました。');
+    }
   }catch(e){
     console.error('エラーが発生しました:', e);
   }
