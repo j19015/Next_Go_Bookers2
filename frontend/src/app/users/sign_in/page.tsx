092579f07f7918d63da5cdfabb6c8bbea567b003
j19015 <j19015@sangi.jp>
2023-07-28 20:45:33 +0900

    sign_in/page.tsx handlesubmit処理内容記述

--- frontend/src/app/users/sign_in/page.tsx
+++ frontend/src/app/users/sign_in/page.tsx
@@ -14,13 +14,26 @@ import { ResponseUser,ServerResponse } from '../../../const/users/interface';
 
 
 const Home = () => {
-  //nameとpasswordを定義
-  const [name,setName]=useState('');
+  //emailとpasswordを定義
+  const [email,setEmail]=useState('');
   const [password,setPassword]=useState('');
 
   //form submitの処理
   const handlesubmit=async(e: FormEvent<HTMLFormElement>)=>{
-
+    //formのデフォルト送信を防止
+    e.preventDefault();
+    //signInUserを実行
+    const res: ResponseUser | ServerResponse = await signInUser({email,password});
+    // ここでサーバーにユーザー情報を送信するなどの処理を行う
+    if ('error' in res) {
+      // エラーが発生した場合
+      console.error('ユーザーのログインに失敗しました。');
+      console.error('Error:', res.error);
+    } else {
+      // エラーが発生しなかった場合（正常なレスポンスの場合）
+      console.log('ユーザーのログインに成功しました！');
+      console.log('User:', res);
+    }
   }
 
   return (
