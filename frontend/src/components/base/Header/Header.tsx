a2aa1e07f8279ea243a8b335742df51ebc7e5a2c
j19015 <j19015@sangi.jp>
2023-07-29 22:45:46 +0900

    header Linkの記述を追加

--- frontend/src/components/base/Header/Header.tsx
+++ frontend/src/components/base/Header/Header.tsx
@@ -1,7 +1,22 @@
-// components/Header.tsx
-import React from 'react';
+//クライアントサイドのレンダリングであることを伝える
+"use client";
+
+import React,{useState,useEffect} from 'react';
+//Linkコンポーネントをimport
+import Link from 'next/link';
+//session情報を取得
+import { sessionConfirm } from '@/features/users/api/session';
 
 const Header = () => {
+
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(()=>{
+    const session_user=sessionConfirm()
+    session_user ? setIsLoggedIn(true): setIsLoggedIn(false);
+  },[])
+
   return (
     <header className="bg-blue-500 p-4">
       <div className="container mx-auto">
@@ -9,9 +24,60 @@ const Header = () => {
           <h1 className="text-white text-xl font-bold">Bookers2</h1>
           <nav>
             <ul className="flex space-x-4 text-white">
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
+              {/* セッションがある場合 */}
+              {isLoggedIn ? (
+                <>
+                  <li>
+                    <Link href="/">
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/about">
+                      About
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/books">
+                      Books
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/users">
+                      Users
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/users/sign_out">
+                      Sign Out
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                // セッションがない場合
+                <>
+                  <li>
+                    <Link href="/">
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/about">
+                      About
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/users/sign_up">
+                      Sign Up
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/users/sign_in">
+                      Sign In
+                    </Link>
+                  </li>
+                </>
+              )}
             </ul>
           </nav>
         </div>
