be36c4e27940cc21e402db31c6b7e5d475aab04b
j19015 <j19015@sangi.jp>
2023-07-31 01:03:03 +0900

    Headerコンポーネントを各ページコンポーネントに追加

--- frontend/src/app/users/page.tsx
+++ frontend/src/app/users/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 //nextでページ遷移を行うため
 import Link from 'next/link';
+//Headerコンポーネとをimport
+import Header from '@/components/base/Header/Header'
 
 const Home = () => {
   return (
     <>
+      <Header></Header>
       <h1>User 一覧ページ</h1>
     </>
   );
--- frontend/src/app/users/sign_up/page.tsx
+++ frontend/src/app/users/sign_up/page.tsx
@@ -14,6 +14,8 @@ import { signUpUser } from '../../../features/users/api/sign_up';
 import { ResponseUser,ServerResponse } from '../../../const/users/interface';
 //セッションを受け取る関数をimport
 import { sessionConfirm } from '@/features/users/api/session';
+//Headerコンポーネとをimport
+import Header from '@/components/base/Header/Header'
 
 
 const Home = () => {
@@ -54,6 +56,7 @@ const Home = () => {
 
   return (
     <>
+      <Header></Header>
       <div className='min-h-screen flex items-center justify-center bg-gray-100'>
         <div className='bg-white p-8 rounded shadow-md w-96'>
           <h1 className="text-3xl font-bold mb-6 text-black">Sign up</h1>
