d7895984f5de26d0e67712376191be17cd554302
j19015 <j19015@sangi.jp>
2023-08-02 11:54:42 +0900

    new book api 作成 & books/index ページのセッション処理

--- frontend/src/app/books/page.tsx
+++ frontend/src/app/books/page.tsx
@@ -8,22 +8,52 @@ import Link from 'next/link';
 import Header from '@/components/base/Header/Header'
 //getBookAllをimport
 import { getBookAll } from '../../features/books/api/get_bookAll';
+//newBookをimort
+import { newBook } from '@/features/books/api/new_book';
 //interfaceをimport
 import { BookList,BookListItem,ServerResponse} from "@/const/books/interface";
+//ResponseUserをimport
+import { ResponseUser } from '@/const/users/interface';
+//sessionを取得する関数をimport
+import { sessionConfirm } from '@/features/users/api/session';
+// Next.js の useRouter フックを使用する
+import { useRouter } from 'next/navigation'; 
 
 const Home = () => {
+
+  //本作成用
+  const [title,setTitle]=useState(null);
+  const [body,setBody]=useState(null);
+  const [session_user,SetSession]=useState<ResponseUser | null>(null)
+
+  // useRouterフックを初期化
+  const router = useRouter(); 
 
   const [bookAll,setBookAll]=useState<BookList>([])
 
   useEffect(() => {
+    //sessionをAPIで取得
+    const session = sessionConfirm();
+    // sessionの情報を保存する
+    getSession(session);
+    
+    if(!session){
+      //sign_inへ飛ばす
+      router.push("/users/sign_in")
+    }
     // ページがロードされた時に本の一覧を取得する
     fetchBooksAll();
   },[]);
 
+  //sessionを取得
+  const getSession = async(session: ResponseUser | null)=>{
+    SetSession(session);
+  } 
+
   // 本の一覧を取得する関数
   const fetchBooksAll = async () => {
     try {
-      //APIのレスポンセ鵜を取得
+      //APIのレスポンスを取得
       const res: BookList | ServerResponse = await getBookAll();
        // resの内容を表示
       console.log('API:', res);
@@ -41,6 +71,17 @@ const Home = () => {
       console.error('Error:', error);
     }
   };
+
+  //本を作成
+  const handleSubmit=async()=>{
+    try{
+      //APIレスポンスを取得
+      //const res: BookListItem | ServerResponse= await newBook()
+
+    }catch{
+
+    }
+  }
 
   return (
     <>
---
+++ frontend/src/features/books/api/new_book.ts
@@ -0,0 +1,35 @@
+import { Book, BookListItem,ServerResponse} from "@/const/books/interface";
+
+export const newBook=async(book: Book)=>{
+  try{
+    // POSTリクエストを使用してユーザー情報をサーバーに送信
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/books`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify(book)
+    });
+
+    // サーバーからのレスポンスに基づいて適切な処理を実行
+    if (response.ok) {
+      // サーバーからのレスポンスを取得
+      const data: BookListItem = await response.json();
+      return data; // 正常なレスポンスの場合はデータを返す
+
+    } else {
+      // サーバーからのレスポンスを取得
+      const data: ServerResponse = await response.json();
+      return data; // エラーレスポンスでもデータを返す（エラーメッセージが含まれる）
+    }
+  } catch (e :any) {
+    return { error: 'エラーが発生しました', message: e.message }; // エラーオブジェクトを返す
+  }
+};
+
+
+
+
+
+
