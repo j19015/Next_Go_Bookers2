290858eb2ac009bc278e24c5a8965be6c96184e1
j19015 <j19015@sangi.jp>
2023-08-05 04:42:06 +0900

    delete_book responseの記述変更

---
+++ frontend/src/features/books/api/delete_book.ts
@@ -0,0 +1,27 @@
+import { BookListItem,ServerResponse} from "@/const/books/interface";
+
+export const deleteBook=async(id: number)=>{
+  try{
+    // POSTリクエストを使用してユーザー情報をサーバーに送信
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/books/${id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      credentials: 'include',
+    });
+
+    // サーバーからのレスポンスを返す
+    const data: ServerResponse = await response.json();
+    return data;
+
+  } catch (e :any) {
+    return { error: 'エラーが発生しました', message: e.message }; // エラーオブジェクトを返す
+  }
+};
+
+
+
+
+
+
