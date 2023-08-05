2c494b43ad25d0ec9a1dbb32110b9ffecb32b4b5
j19015 <j19015@sangi.jp>
2023-08-06 02:35:33 +0900

    users/index fecthの記述やuseEffectの記述を追加

--- frontend/src/app/users/page.tsx
+++ frontend/src/app/users/page.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+"use client"
+
+import React,{useEffect,useState} from 'react';
 //nextでページ遷移を行うため
 import Link from 'next/link';
 //Headerコンポーネとをimport
 import Header from '@/components/base/Header/Header'
+//Userの型をimport
+import { UserList,ResponseUser,ServerResponse } from '@/const/users/interface';
+//getUserAllをimport
+import { getUserAll } from '@/features/users/api/get_userAll';
 
 const Home = () => {
+  
+  const [userAll,setUserAll] = useState<UserList | null>(null);
+
+  const fetchUsers = async()=>{
+    try{
+      const res = await getUserAll();
+      if ('error' in res){
+        console.log("API error:",res.error);
+      }else{
+        setUserAll(res);
+      }
+    }catch(error){
+      console.log("error",error);
+    }
+  }
+
+  useEffect(()=>{
+    fetchUsers();
+  },[])
+
   return (
     <>
       <Header></Header>
