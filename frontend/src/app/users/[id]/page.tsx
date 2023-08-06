b48436c42cbf8417c84f8d070603abe3605f4d4b
j19015 <j19015@sangi.jp>
2023-08-07 02:16:07 +0900

    users/showの処置を追加

--- frontend/src/app/users/[id]/page.tsx
+++ frontend/src/app/users/[id]/page.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 //nextでページ遷移を行うため
 import Link from 'next/link';
 //Headerコンポーネとをimport
 import Header from '@/components/base/Header/Header'
+//User型をimport
+import { ResponseUser, ServerResponse } from '@/const/users/interface';
+//URLパラメータ取得用
+import { useParams } from 'next/navigation';
+//getUserをimport
+import { getUser } from '@/features/users/api/get_user';
+
 
 const Home = () => {
+
+  //user情報を格納するようの変数
+  const [user,setUser] = useState<ResponseUser | null>(null);
+  //URLパラメータ取得
+  const params = useParams();
+  //idを抽出
+  const {id} = params;
+
+  const fetchUser = async () => {
+    try{
+      if(id && isNaN(Number(id))){
+        const res :ResponseUser | ServerResponse= await getUser(Number(id));
+        if ('error' in res){
+          console.log("error",res.error);
+        }else{
+
+        }
+      }else{
+        console.log("error","そのユーザは存在しません。");
+      }
+    }catch(error){
+      console.log("error:",error);
+    }
+  }
+  
+
   return (
     <>
       <Header></Header>
