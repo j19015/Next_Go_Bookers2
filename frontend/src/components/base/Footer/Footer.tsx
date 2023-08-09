a797f60bf3781643f379560e8e19753ce0a1aa12
j19015 <j19015@sangi.jp>
2023-08-10 02:19:16 +0900

    footerのレイアウトを作成

---
+++ frontend/src/components/base/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+// Footer.tsx
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-300 py-4 text-center">
+      <p className="text-gray-600">© 2023 Your Company. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
