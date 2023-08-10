e2bc027edcc811db5b1bc34f3a7f780d99486eb7
j19015 <j19015@sangi.jp>
2023-08-11 02:06:49 +0900

    footerが一番下に固定ではなく、ページの一番下に表示するように変更

--- frontend/src/components/base/Footer/Footer.tsx
+++ frontend/src/components/base/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-300 py-4 text-center fixed bottom-0 w-full">
+    <footer className="bg-gray-300 py-4 text-center w-full">
       <p className="text-gray-600">© 2023 Your Company. All rights reserved.</p>
     </footer>
   );
